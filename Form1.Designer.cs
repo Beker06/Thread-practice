020a8be9e783f22794c4ab2b01f79562db6a1b87
Eliam Vazquez <bosc790@gmail.com>
2023-01-24 23:30:17 -0800

    V1.0.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace PracticaHilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.personaje4 = new System.Windows.Forms.PictureBox();
+            this.personaje3 = new System.Windows.Forms.PictureBox();
+            this.personaje2 = new System.Windows.Forms.PictureBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.personaje1 = new System.Windows.Forms.PictureBox();
+            this.personaje5 = new System.Windows.Forms.PictureBox();
+            this.btnPause = new System.Windows.Forms.PictureBox();
+            this.btnRestart = new System.Windows.Forms.PictureBox();
+            this.lblGanador = new System.Windows.Forms.Label();
+            this.pbGanador = new System.Windows.Forms.PictureBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.personaje4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRestart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGanador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // personaje4
+            // 
+            this.personaje4.BackColor = System.Drawing.Color.Transparent;
+            this.personaje4.BackgroundImage = global::PracticaHilos.Properties.Resources.cheems1;
+            this.personaje4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.personaje4.Location = new System.Drawing.Point(49, 413);
+            this.personaje4.Name = "personaje4";
+            this.personaje4.Size = new System.Drawing.Size(97, 94);
+            this.personaje4.TabIndex = 0;
+            this.personaje4.TabStop = false;
+            // 
+            // personaje3
+            // 
+            this.personaje3.BackColor = System.Drawing.Color.Transparent;
+            this.personaje3.BackgroundImage = global::PracticaHilos.Properties.Resources.shrek;
+            this.personaje3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.personaje3.Location = new System.Drawing.Point(39, 466);
+            this.personaje3.Name = "personaje3";
+            this.personaje3.Size = new System.Drawing.Size(94, 99);
+            this.personaje3.TabIndex = 1;
+            this.personaje3.TabStop = false;
+            // 
+            // personaje2
+            // 
+            this.personaje2.BackColor = System.Drawing.Color.Transparent;
+            this.personaje2.BackgroundImage = global::PracticaHilos.Properties.Resources.mario;
+            this.personaje2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.personaje2.Location = new System.Drawing.Point(39, 513);
+            this.personaje2.Name = "personaje2";
+            this.personaje2.Size = new System.Drawing.Size(88, 90);
+            this.personaje2.TabIndex = 2;
+            this.personaje2.TabStop = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.White;
+            this.btnIniciar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciar.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrange;
+            this.btnIniciar.FlatAppearance.BorderSize = 2;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnIniciar.Image = global::PracticaHilos.Properties.Resources.bullet__2_;
+            this.btnIniciar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnIniciar.Location = new System.Drawing.Point(397, 320);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(242, 86);
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.Text = " Iniciar";
+            this.btnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // personaje1
+            // 
+            this.personaje1.BackColor = System.Drawing.Color.Transparent;
+            this.personaje1.BackgroundImage = global::PracticaHilos.Properties.Resources.gary;
+            this.personaje1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.personaje1.Location = new System.Drawing.Point(39, 591);
+            this.personaje1.Name = "personaje1";
+            this.personaje1.Size = new System.Drawing.Size(79, 60);
+            this.personaje1.TabIndex = 4;
+            this.personaje1.TabStop = false;
+            // 
+            // personaje5
+            // 
+            this.personaje5.BackColor = System.Drawing.Color.Transparent;
+            this.personaje5.BackgroundImage = global::PracticaHilos.Properties.Resources.creeper;
+            this.personaje5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.personaje5.Location = new System.Drawing.Point(58, 352);
+            this.personaje5.Name = "personaje5";
+            this.personaje5.Size = new System.Drawing.Size(88, 108);
+            this.personaje5.TabIndex = 5;
+            this.personaje5.TabStop = false;
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.Transparent;
+            this.btnPause.BackgroundImage = global::PracticaHilos.Properties.Resources.pause;
+            this.btnPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPause.Location = new System.Drawing.Point(73, 12);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(50, 50);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.TabStop = false;
+            this.btnPause.Visible = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.BackgroundImage = global::PracticaHilos.Properties.Resources.restart;
+            this.btnRestart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestart.Location = new System.Drawing.Point(12, 12);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(50, 50);
+            this.btnRestart.TabIndex = 7;
+            this.btnRestart.TabStop = false;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // lblGanador
+            // 
+            this.lblGanador.AutoSize = true;
+            this.lblGanador.BackColor = System.Drawing.Color.Transparent;
+            this.lblGanador.Font = new System.Drawing.Font("Impact", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGanador.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblGanador.Location = new System.Drawing.Point(317, 30);
+            this.lblGanador.Name = "lblGanador";
+            this.lblGanador.Size = new System.Drawing.Size(410, 117);
+            this.lblGanador.TabIndex = 8;
+            this.lblGanador.Text = "GANADOR";
+            this.lblGanador.Visible = false;
+            // 
+            // pbGanador
+            // 
+            this.pbGanador.BackColor = System.Drawing.Color.Transparent;
+            this.pbGanador.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbGanador.Location = new System.Drawing.Point(318, 138);
+            this.pbGanador.Name = "pbGanador";
+            this.pbGanador.Size = new System.Drawing.Size(422, 438);
+            this.pbGanador.TabIndex = 9;
+            this.pbGanador.TabStop = false;
+            this.pbGanador.Visible = false;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAceptar.FlatAppearance.BorderColor = System.Drawing.Color.OrangeRed;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnAceptar.Location = new System.Drawing.Point(457, 591);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(134, 45);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Visible = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::PracticaHilos.Properties.Resources.fondocarrera;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1088, 692);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.pbGanador);
+            this.Controls.Add(this.lblGanador);
+            this.Controls.Add(this.personaje1);
+            this.Controls.Add(this.personaje2);
+            this.Controls.Add(this.personaje3);
+            this.Controls.Add(this.personaje4);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.personaje5);
+            this.Controls.Add(this.btnIniciar);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Carreritas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.personaje4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personaje5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRestart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGanador)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox personaje4;
+        private System.Windows.Forms.PictureBox personaje3;
+        private System.Windows.Forms.PictureBox personaje2;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.PictureBox personaje1;
+        private System.Windows.Forms.PictureBox personaje5;
+        private System.Windows.Forms.PictureBox btnPause;
+        private System.Windows.Forms.PictureBox btnRestart;
+        private System.Windows.Forms.Label lblGanador;
+        private System.Windows.Forms.PictureBox pbGanador;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+using System.IO;
+
+namespace PracticaHilos
+{
+    public partial class Form1 : Form
+    {
+        int pauseState;
+        bool carrera = true;
+        int p = 0;
+        int a = 639;
+        int b = 639;
+        int c = 639;
+        int d = 649;
+        int e = 658;
+
+        Stream Sonido;
+        SoundPlayer player;
+
+
+        Thread proceso_1;
+        Thread proceso_2;
+        Thread proceso_3;
+        Thread proceso_4;
+        Thread proceso_5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pauseState = 1;
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            proceso_1 = new Thread(new ThreadStart(funcion_1));
+            proceso_2 = new Thread(new ThreadStart(funcion_2));
+            proceso_3 = new Thread(new ThreadStart(funcion_3));
+            proceso_4 = new Thread(new ThreadStart(funcion_4));
+            proceso_5 = new Thread(new ThreadStart(funcion_5));
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            Sonido = PracticaHilos.Properties.Resources.Disparo;
+            player = new SoundPlayer(Sonido);
+            player.Play();
+            proceso_1 = new Thread(new ThreadStart(funcion_1));
+            proceso_2 = new Thread(new ThreadStart(funcion_2));
+            proceso_3 = new Thread(new ThreadStart(funcion_3));
+            proceso_4 = new Thread(new ThreadStart(funcion_4));
+            proceso_5 = new Thread(new ThreadStart(funcion_5));
+
+            timer1.Start();
+
+            btnIniciar.Visible = false;
+            btnRestart.Visible = true;
+            btnPause.Visible = true;
+            try
+            {
+                proceso_1.Start();
+                proceso_2.Start();
+                proceso_3.Start();
+                proceso_4.Start();
+                proceso_5.Start();
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (pauseState == 1)
+            {
+                pauseState = 2;
+                btnPause.BackgroundImage = PracticaHilos.Properties.Resources.play;
+                Suspender();
+            }
+            else
+            {
+                pauseState = 1;
+                btnPause.BackgroundImage = PracticaHilos.Properties.Resources.pause;
+                Reanudar();
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            if(pauseState == 1)
+            {
+                proceso_1.Abort();
+                proceso_2.Abort();
+                proceso_3.Abort();
+                proceso_4.Abort();
+                proceso_5.Abort();
+
+                CrearHilos();
+
+                btnIniciar.Visible = true;
+                btnRestart.Visible = false;
+                btnPause.Visible = false;
+
+                ReiniciarPersonajes();
+            }
+            else
+            {
+                Reanudar();
+
+                proceso_1.Abort();
+                proceso_2.Abort();
+                proceso_3.Abort();
+                proceso_4.Abort();
+                proceso_5.Abort();
+
+                CrearHilos();
+
+                btnIniciar.Visible = true;
+                btnRestart.Visible = false;
+                btnPause.Visible = false;
+
+                ReiniciarPersonajes();
+            }
+        }
+
+        public void funcion_1()
+        {
+            while (true)
+            {
+                if (personaje1.Left <= a)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(r.Next(53, 300));
+                    Random r2 = new Random();
+
+                    personaje1.Left = personaje1.Left + r2.Next(2, 10);
+                }
+                else
+                {
+                    p = 1;
+                    carrera = false;
+                }
+            }
+        }
+
+        public void funcion_2()
+        {
+            while (true)
+            {
+                if (personaje2.Left <= b)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(r.Next(53, 300));
+                    Random r2 = new Random();
+
+                    personaje2.Left = personaje2.Left + r2.Next(2, 10);
+                }
+                else
+                {
+                    p = 2;
+                    carrera = false;
+                }
+            }
+        }
+
+        public void funcion_3()
+        {
+            while (true)
+            {
+                if (personaje3.Left <= c)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(r.Next(53, 300));
+                    Random r2 = new Random();
+
+                    personaje3.Left = personaje3.Left + r2.Next(2, 10);
+                }
+                else
+                {
+                    p = 3;
+                    carrera = false;
+                }
+            }
+        }
+
+        public void funcion_4()
+        {
+            while (true)
+            {
+                if(personaje4.Left <= d)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(r.Next(53, 300));
+                    Random r2 = new Random();
+
+                    personaje4.Left = personaje4.Left + r2.Next(2, 10);
+                }
+                else 
+                {
+                    p = 4;
+                    carrera = false;
+                }
+            }
+        }
+
+        public void funcion_5()
+        {
+            while (true)
+            {
+                if(personaje5.Left <= e)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(r.Next(53, 300));
+                    Random r2 = new Random();
+
+                    personaje5.Left = personaje5.Left + r2.Next(2, 10);
+                }
+                else
+                {
+                    p = 5;
+                    carrera = false;
+                }
+            }
+        }
+
+        public void CrearHilos()
+        {
+            Thread proceso_1;
+            Thread proceso_2;
+            Thread proceso_3;
+            Thread proceso_4;
+            Thread proceso_5;
+        }
+
+        public void ReiniciarPersonajes()
+        {
+            personaje1.Left = 39;
+            personaje2.Left = 39;
+            personaje3.Left = 39;
+            personaje4.Left = 49;
+            personaje5.Left = 58;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            p = 0;
+            carrera = true;
+
+            lblGanador.Visible = false;
+            pbGanador.Visible = false;
+            btnAceptar.Visible = false;
+            btnRestart.Visible = false;
+            btnPause.Visible = false;
+            btnIniciar.Visible = true;
+
+            ReiniciarPersonajes();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (carrera == false)
+            {
+                Suspender();
+                Sonido = PracticaHilos.Properties.Resources.Ganador;
+                player = new SoundPlayer(Sonido);
+                player.Play();
+
+                lblGanador.Visible = true;
+                pbGanador.Visible = true;
+                btnAceptar.Visible = true;
+                btnRestart.Visible = false;
+                btnPause.Visible = false;
+
+                if (p == 1)
+                {
+                    pbGanador.BackgroundImage = PracticaHilos.Properties.Resources.gary;
+                }
+                else if(p == 2)
+                {
+                    pbGanador.BackgroundImage = PracticaHilos.Properties.Resources.mario;
+                }
+                else if (p == 3)
+                {
+                    pbGanador.BackgroundImage = PracticaHilos.Properties.Resources.shrek;
+                }
+                else if (p == 4)
+                {
+                    pbGanador.BackgroundImage = PracticaHilos.Properties.Resources.cheems1;
+                }
+                else if (p == 5)
+                {
+                    pbGanador.BackgroundImage = PracticaHilos.Properties.Resources.creeper;
+                }
+
+                timer1.Stop();
+            }
+        }
+
+        public void Suspender()
+        {
+            proceso_1.Suspend();
+            proceso_2.Suspend();
+            proceso_3.Suspend();
+            proceso_4.Suspend();
+            proceso_5.Suspend();
+        }
+
+        public void Reanudar()
+        {
+            proceso_1.Resume();
+            proceso_2.Resume();
+            proceso_3.Resume();
+            proceso_4.Resume();
+            proceso_5.Resume();
+        }
+    }
+}
